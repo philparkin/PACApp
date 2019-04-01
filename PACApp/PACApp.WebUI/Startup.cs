bef6973e1fef6c2d8c241ac2245d52b2fbaa5cf2
Phil Parkin <phil@pparkin.co.uk>
2019-04-01 10:43:09 +0100

    Initial Solution Setup

---
+++ PACApp/PACApp.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PACApp.WebUI.Startup))]
+namespace PACApp.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
