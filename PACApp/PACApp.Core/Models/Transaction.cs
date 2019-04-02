937716cae822108627c027ed92861184dfcbf744
Phil Parkin <phil@pparkin.co.uk>
2019-04-02 10:54:14 +0100

    Transaction Model and InMemory Transaction Repository Built

---
+++ PACApp/PACApp.Core/Models/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PACApp.Core.Models
+{
+    public class Transaction
+    {
+        public string Id { get; set; }
+      
+        public DateTime InvoiceDate { get; set; }
+        public DateTime PaidDate { get; set; }
+
+        [Range(0, 9000000)]
+        public decimal Amount { get; set; }
+
+        public Transaction()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
